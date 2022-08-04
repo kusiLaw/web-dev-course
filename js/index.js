b61c524f4abe94f5a9b8b01913f02e6542fa7dc0
kusi <lawcubegsm@gmail.com>
2022-08-04 12:13:49 +0000

    Fix linter js and html linter error

--- js/index.js
+++ js/index.js
@@ -1,28 +1,28 @@
-let speakers = [
+const speakers = [
   {
-    picture: "./images/speaker_02.png",
-    name: "Lila Tretiklov",
-    work: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
-    extra: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime quae hic eos molestias unde! Minus rerum voluptates corporis aut recusandae dolores quo quis, officiis,repellat! Molestias, illo."
+    picture: './images/speaker_02.png',
+    name: 'Lila Tretiklov',
+    work: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+    extra: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime quae hic eos molestias unde! Minus rerum voluptates corporis aut recusandae dolores quo quis, officiis,repellat! Molestias, illo.',
   },
   {
-    picture: "./images/speaker_04.png",
-    name: "Kenneth Nii",
-    work: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
-    extra: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime quae hic eos molestias unde! Minus rerum voluptates corporis aut recusandae dolores quo quis, officiis,repellat! Molestias, illo."
+    picture: './images/speaker_04.png',
+    name: 'Kenneth Nii',
+    work: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+    extra: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime quae hic eos molestias unde! Minus rerum voluptates corporis aut recusandae dolores quo quis, officiis,repellat! Molestias, illo.',
   },
   {
-    picture: "./images/speaker_05.png",
-    name: "Afua Koobi",
-    work: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
-    extra: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime quae hic eos molestias unde! Minus rerum voluptates corporis aut recusandae dolores quo quis, officiis,repellat! Molestias, illo."
+    picture: './images/speaker_05.png',
+    name: 'Afua Koobi',
+    work: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+    extra: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime quae hic eos molestias unde! Minus rerum voluptates corporis aut recusandae dolores quo quis, officiis,repellat! Molestias, illo.',
   },
   {
-    picture: "./images/speaker_06.png",
-    name: "Yaw Appesi",
-    work: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
-    extra: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime quae hic eos molestias unde! Minus rerum voluptates corporis aut recusandae dolores quo quis, officiis,repellat! Molestias, illo."
-  }
+    picture: './images/speaker_06.png',
+    name: 'Yaw Appesi',
+    work: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+    extra: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime quae hic eos molestias unde! Minus rerum voluptates corporis aut recusandae dolores quo quis, officiis,repellat! Molestias, illo.',
+  },
 
 ];
 
@@ -40,82 +40,77 @@ function createMyElement(el, cl = '', ids = '') {
 }
 
 function createPopup(obj) {
-  let ps = createMyElement("p")
-  ps.textContent= obj.extra
-  let extra = createMyElement('div', 'extra')
-  extra.appendChild(ps)
+  const ps = createMyElement('p');
+  ps.textContent = obj.extra;
+  const extra = createMyElement('div', 'extra');
+  extra.appendChild(ps);
 
-  let p = createMyElement("p")
-  p.textContent= obj.work
-  let work = createMyElement('div', 'work')
-  work.appendChild(p)
+  const p = createMyElement('p');
+  p.textContent = obj.work;
+  const work = createMyElement('div', 'work');
+  work.appendChild(p);
 
-  let h3 = createMyElement("h3")
-  h3.textContent = obj.name
-  let name = createMyElement('div', 'name')
-  name.appendChild(h3)
+  const h3 = createMyElement('h3');
+  h3.textContent = obj.name;
+  const name = createMyElement('div', 'name');
+  name.appendChild(h3);
 
-  let about = createMyElement('div', 'about')
-  about.appendChild(name)
-  about.appendChild(work)
+  const about = createMyElement('div', 'about');
+  about.appendChild(name);
+  about.appendChild(work);
 
-  let lecturerInfo = createMyElement('div', 'lecturer-info')
-  lecturerInfo.appendChild(about)
-  lecturerInfo.appendChild(extra)
+  const lecturerInfo = createMyElement('div', 'lecturer-info');
+  lecturerInfo.appendChild(about);
+  lecturerInfo.appendChild(extra);
 
-
-  let Img = createMyElement('img', 'lect-img')
+  const Img = createMyElement('img', 'lect-img');
   Img.setAttribute('src', obj.picture);
-  Img.setAttribute("alt", obj.name)
-  let lectImgWrap = createMyElement('div', 'lect-img-wrap')
+  Img.setAttribute('alt', obj.name);
+  const lectImgWrap = createMyElement('div', 'lect-img-wrap');
   lectImgWrap.appendChild(Img);
 
-  let lecturer = createMyElement('div', 'lecturer')
-  lecturer.appendChild(lectImgWrap)
-  lecturer.appendChild(lecturerInfo)
-  let lecturers = createMyElement('div', 'lecturers')
-  lecturers.appendChild(lecturer)
+  const lecturer = createMyElement('div', 'lecturer');
+  lecturer.appendChild(lectImgWrap);
+  lecturer.appendChild(lecturerInfo);
+  const lecturers = createMyElement('div', 'lecturers');
+  lecturers.appendChild(lecturer);
 
-  return lecturers
+  return lecturers;
 }
 
-function insertToPage(obj, parent){
-  for(let i =0; i< obj.length; i+= 1){
-    parent.appendChild(createPopup(obj[i]))
+function insertToPage(obj, parent) {
+  for (let i = 0; i < obj.length; i += 1) {
+    parent.appendChild(createPopup(obj[i]));
   }
 }
 
 let hasPopulate = false;
-let mainspeakers= document.querySelector(".lecturers_wrapper")
+const mainspeakers = document.querySelector('.lecturers_wrapper');
 
-
-let btn = document.querySelector(".more")
-btn.addEventListener("click", (event)=>{
-  btn.style.display = 'none'; 
-  insertToPage(speakers, mainspeakers)
- 
+const btn = document.querySelector('.more');
+btn.addEventListener('click', (event) => {
+  btn.style.display = 'none';
+  insertToPage(speakers, mainspeakers);
   hasPopulate = true;
-})
+  event.stopPropagation();
+});
 
-
-window.onload = function(event) {
-
-  if(window.innerWidth >= 768){
-    btn.style.display = 'none'; 
-    if (!hasPopulate){
-      insertToPage(speakers, mainspeakers)
-      hasPopulate = true
+window.onload = function name2(event) {
+  if (window.innerWidth >= 768) {
+    btn.style.display = 'none';
+    if (!hasPopulate) {
+      insertToPage(speakers, mainspeakers);
+      hasPopulate = true;
     }
   }
-   
-}
+  event.stopPropagation();
+};
 
-window.onresize = function(event) {
-
-  if (!hasPopulate){
-    btn.style.display = 'none'; 
-    insertToPage(speakers, mainspeakers)
-    hasPopulate = true
+window.onresize = function name(event) {
+  if (!hasPopulate) {
+    btn.style.display = 'none';
+    insertToPage(speakers, mainspeakers);
+    hasPopulate = true;
   }
-   
-}
+  event.stopPropagation();
+};
--- js/nav.js
+++ js/nav.js
@@ -1,56 +1,52 @@
+// toggle
+const toggle = document.querySelector('#toggle-close');
+const navContent = document.querySelector('.nav-content');
+const expand = document.querySelector('#close-nav');
 
-//toggle
-let toggle= document.querySelector("#toggle-close")
-let navContent= document.querySelector(".nav-content")
-let expand= document.querySelector("#close-nav")
-
-
-toggle.addEventListener("click", (event) => {
-  navContent.style.display = "none"
+toggle.addEventListener('click', (event) => {
+  navContent.style.display = 'none';
   event.stopPropagation();
-})
+});
 
-expand.addEventListener("click", (event) => {
-  navContent.style.display = "flex"
+expand.addEventListener('click', (event) => {
+  navContent.style.display = 'flex';
   event.stopPropagation();
-})
+});
 
-
-document.querySelector(".nav-items-content").addEventListener("click", (event) => {
-  let inner = document.querySelector(".nav-nestd-list")
-  let pointer = document.querySelector("#pointer")
+document.querySelector('.nav-items-content').addEventListener('click', (event) => {
+  const inner = document.querySelector('.nav-nestd-list');
+  const pointer = document.querySelector('#pointer');
   inner.classList.toggle('toggle');
   pointer.classList.toggle('toggle');
   // navContent.style.display = "none";
-  console.log("am click")
   event.stopPropagation();
-})
+});
 
-
-document.querySelector("#ruby").addEventListener("click", (event) => {
-  navContent.style.display = "none";
+document.querySelector('#ruby').addEventListener('click', (event) => {
+  navContent.style.display = 'none';
   event.stopPropagation();
-})
-document.querySelector("#CSS").addEventListener("click", (event) => {
-  navContent.style.display = "none";
+});
+document.querySelector('#CSS').addEventListener('click', (event) => {
+  navContent.style.display = 'none';
   event.stopPropagation();
-})
-document.querySelector("#django").addEventListener("click", (event) => {
-  navContent.style.display = "none";
+});
+document.querySelector('#django').addEventListener('click', (event) => {
+  navContent.style.display = 'none';
   event.stopPropagation();
-})
-document.querySelector("#react").addEventListener("click", (event) => {
-  navContent.style.display = "none";
+});
+document.querySelector('#react').addEventListener('click', (event) => {
+  navContent.style.display = 'none';
   event.stopPropagation();
-})
-document.querySelector("#home").addEventListener("click", (event) => {
-  navContent.style.display = "none"
-})
-document.querySelector("#about").addEventListener("click", (event) => {
-  navContent.style.display = "none";
+});
+document.querySelector('#home').addEventListener('click', (event) => {
+  navContent.style.display = 'none';
   event.stopPropagation();
-})
-document.querySelector("#sponsor").addEventListener("click", (event) => {
-  navContent.style.display = "none"
+});
+document.querySelector('#about').addEventListener('click', (event) => {
+  navContent.style.display = 'none';
   event.stopPropagation();
-})
+});
+document.querySelector('#sponsor').addEventListener('click', (event) => {
+  navContent.style.display = 'none';
+  event.stopPropagation();
+});
